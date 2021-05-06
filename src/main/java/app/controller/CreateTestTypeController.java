a9d9293aefcc9824699176391d86180fed6dc08c
Francisco Redol (1201239) <1201239@isep.ipp.pt>
2021-05-06 12:56:20 +0100

    CreateTestTypeController.java da US9 criado.

---
+++ src/main/java/app/controller/CreateTestTypeController.java
@@ -0,0 +1,16 @@
+package app.controller;
+
+public class CreateTestTypeController {
+
+    public boolean createTestType8(String code, String Description, String CollectingMethod){
+
+    }
+
+    public boolean validateTestType(){
+
+    }
+
+    public boolean saveTestType(){
+        
+    }
+}
