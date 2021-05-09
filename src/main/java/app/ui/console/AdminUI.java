6e11e446e7489c3b16f5fd883de74cb92ef09d99
Francisco Redol (1201239) <1201239@isep.ipp.pt>
2021-05-09 13:11:13 +0100

    Alteração do ADMIN UI

--- src/main/java/app/ui/console/AdminUI.java
+++ src/main/java/app/ui/console/AdminUI.java
@@ -18,9 +18,9 @@ public class AdminUI implements Runnable{
     public void run()
     {
         List<MenuItem> options = new ArrayList<MenuItem>();
-        options.add(new MenuItem("Specify a new type of test", new ShowTextUI("You have chosen to create a new type of test.")));
+        options.add(new MenuItem("Specify a new type of test", new CreateTestTypeUI()));
         options.add(new MenuItem("Specify a new parameter", new ShowTextUI("You have chosen to specify a new parameter.")));
-        options.add(new MenuItem("Specify a new parameter category", new ShowTextUI("You have chosen to specify a new parameter category.")));
+        options.add(new MenuItem("Specify a new parameter category", new CreateParameterCategoryUI()));
         options.add(new MenuItem("Register a new employee", new RegisterNewEmployeeUI()));
 
         int option = 0;
