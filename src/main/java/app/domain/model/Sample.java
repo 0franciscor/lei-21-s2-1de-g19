d6fe3b38f531d84e316cfece152036fa4617e66f
ritalello1201240 <1201240@isep.ipp.pt>
2021-05-26 20:03:33 +0100

    [Implementation] - Implementação da classe Sample

---
+++ src/main/java/app/domain/model/Sample.java
@@ -0,0 +1,19 @@
+package app.domain.model;
+
+import net.sourceforge.barbecue.Barcode;
+
+public class Sample {
+
+    private Barcode barcode;
+
+    public Sample(Barcode barcode){
+        this.barcode=barcode;
+    }
+
+    public Sample(){
+    }
+
+    public Barcode getBarcode(){
+        return this.barcode;
+    }
+}
