56b895da394b722cd2b608dc6b01a70c7d402eb3
Francisco Redol (1201239) <1201239@isep.ipp.pt>
2021-05-30 18:17:33 +0100

    [US15] - Acréscimo de comentários na class Notification.

--- src/main/java/app/domain/model/Notification.java
+++ src/main/java/app/domain/model/Notification.java
@@ -4,18 +4,40 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Date;
 
+
+/**
+ * The notification service.
+ *
+ * @author Francisco Redol
+ */
 public class Notification {
 
+    /**
+     * Responsible for printing to a file.
+     */
     private PrintWriter out;
 
+    /**
+     * @throws FileNotFoundException
+     *
+     * The notification builder.
+     */
     public Notification() throws FileNotFoundException {
         out = new PrintWriter("emailAndSMSMessages.txt");
     }
 
+    /**
+     * @param message The message they want to print.
+     *
+     * The method responsible for writing to a file.
+     */
     public void writeToFile(String message) {
         out.println(new Date() + ": " + message);
     }
 
+    /**
+     * Method responsible for closing the PrintWriter
+     */
     public void close(){
         out.close();
     }
