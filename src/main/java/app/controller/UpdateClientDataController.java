47071be8d90f0b22ba2f9045b0d40822b7e90188
1201238 <1201238@isep.ipp.pt>
2021-06-01 16:30:52 +0100

    [US02] - Implementação da UI e do Controller relativos à US2

--- src/main/java/app/controller/UpdateClientDataController.java
+++ src/main/java/app/controller/UpdateClientDataController.java
@@ -1,4 +1,71 @@
 package app.controller;
 
+import app.domain.model.Client;
+import app.domain.model.Company;
+import auth.domain.store.ClientStore;
+import auth.mappers.ClientMapper;
+import auth.mappers.dto.ClientDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents the controller that serves at intermediary between the UI and the domain layer.
+ *
+ * @author Eduardo Gonçalves
+ */
 public class UpdateClientDataController {
+
+
+    /**
+     * The controller's App object.
+     */
+    private App app;
+
+    /**
+     *  The controller's Company.
+     */
+    private Company company;
+
+    /**
+     * The controller's Client store.
+     */
+    public ClientStore clientstore;
+
+    /**
+     * Builds a UpdateClientDataController without receiving parameters.
+     */
+    public UpdateClientDataController () {
+        this.app = App.getInstance();
+        this.company = app.getCompany();
+        this.clientstore = company.getClientStore();
+
+    }
+
+    /**
+     * Returns a client Dto that receives the Tax Identification number as a parameter.
+     *
+     * @param TIN Tax Identification number
+     * @return client Dto that receives the citizen card number as a parameter.
+     */
+    public Client getClient (String TIN) {
+
+        ClientStore store = company.getClientStore();
+        Client cl = store.getClient(TIN);
+
+        return cl;
+
+    }
+
+
+    public List<String> getDataToUpdate (Client cl){
+
+        ClientStore store = company.getClientStore();
+        List<String> listDataToUpdate = store.getDataToUpdate(cl);
+
+        return listDataToUpdate;
+
+    }
+
+
 }
--- src/main/java/app/ui/console/UpdateClientDataUI.java
+++ src/main/java/app/ui/console/UpdateClientDataUI.java
@@ -1,6 +1,11 @@
 package app.ui.console;
 
 import app.controller.UpdateClientDataController;
+import app.domain.model.Client;
+import app.ui.console.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Represents update client data UI.
@@ -25,5 +30,83 @@ public class UpdateClientDataUI implements Runnable {
 
     public void run() {
 
+        System.out.printf("\n--- UPDATE CLIENT'S DATA ---");
+        String TIN = Utils.readLineFromConsole("\nType the Client's Tax Identification Number:");
+
+        Client cl;
+
+        try{
+            cl = ctrl.getClient(TIN);
+        }catch (Exception e){
+            System.out.println("\nInvalid Tax Identification Number or the client has not been registered yet.");
+            return;
+        }
+
+        if (cl != null){
+
+            boolean confirmation = Utils.confirm(String.format("Is this information right? If so type s, if not type n. \n\n Citizen card number: %s \n National Healthcare Service number (NHS): %s " +
+                    "\n Birth date: %s \n Sex: %s \n Tax Identification number (TIN): %s \n Phone number: %s \n email: %s \n name: %s ", cl.getCitizenID(), cl.getNhsID(), cl.getBirthDate(), cl.getSex(), cl.getTIN(), cl.getPhoneNumber(), cl.getEmail(), cl.getName()));
+
+            if (confirmation){
+
+                List<String> dataToUpdate = ctrl.getDataToUpdate(cl);
+
+                System.out.println("\n--- DATA YOU CAN UPDATE ---");
+
+                List<String> options = new ArrayList<>();
+                options.add("Sex");
+                options.add("Phone number");
+                options.add("Email");
+                options.add("Name");
+
+                int option = Utils.showAndSelectIndex(options,"Select an option from the list.");
+
+                mudarDados(option,cl);
+
+            }
+        }
+    }
+
+    public void mudarDados (int option, Client client){
+
+        switch (option){
+            case 0:
+                System.out.println("Old sex:" + client.getSex());
+                boolean confirmation = Utils.confirm("Change sex?");
+                if (confirmation){
+                    boolean aux = false;
+                    do{
+                        try {
+                            client.setSex(Utils.readLineFromConsole("Updated sex:"));
+                        } catch (Exception e) {
+                            System.out.println("Invalid sex.");
+                            aux = true;
+                        }
+                    }while(!aux);
+
+                }
+                break;
+            case 1:
+                System.out.println("Old phone number:" + client.getPhoneNumber());
+                boolean confirmation1 = Utils.confirm("Change phone number?");
+                if (confirmation1){
+                    client.setSex(Utils.readLineFromConsole("Updated phone number:"));
+                }
+                break;
+            case 2:
+                System.out.println("Old email:" + client.getEmail());
+                boolean confirmation2 = Utils.confirm("Change email?");
+                if (confirmation2){
+                    client.setSex(Utils.readLineFromConsole("Updated email:"));
+                }
+                break;
+            case 3:
+                System.out.println("Old name:" + client.getName());
+                boolean confirmation3 = Utils.confirm("Change name?");
+                if (confirmation3){
+                    client.setSex(Utils.readLineFromConsole("Updated name:"));
+                }
+                break;
+        }
     }
 }
