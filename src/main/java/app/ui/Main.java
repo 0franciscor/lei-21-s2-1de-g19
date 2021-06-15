95d7b67537a4f5208588ff1c18bdbe9b664c3dfd
Alexandre Soares <1201234@isep.ipp.pt>
2021-06-15 10:55:07 +0100

    Implementação de código da US19.

--- src/main/java/app/ui/Main.java
+++ src/main/java/app/ui/Main.java
@@ -6,6 +6,7 @@ import app.domain.model.SendNHSReportTimerTask;
 import app.ui.console.MainMenuUI;
 
 import java.sql.Time;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
 
@@ -20,8 +21,12 @@ public class Main {
         try
         {
             SendNHSReportTimerTask task1 = new SendNHSReportTimerTask();
+            Calendar data = Calendar.getInstance();
+            data.set(Calendar.HOUR_OF_DAY, 6);
+            data.set(Calendar.MINUTE, 0);
+            data.set(Calendar.SECOND,0);
             Timer timer = new Timer();
-            timer.schedule(task1, );
+            timer.schedule(task1, data.getTime(), 1000*60*60*24);
             MainMenuUI menu = new MainMenuUI();
             menu.run();
             Notification notification = App.getInstance().getCompany().getNotificationService();
