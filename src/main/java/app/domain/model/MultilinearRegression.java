10b9c3cf846bfd80ec583ef27b735985bf9f0d6f
ritalello1201240 <1201240@isep.ipp.pt>
2021-06-18 18:31:03 +0100

    [US18] - library do multilinear

--- src/main/java/app/domain/model/MultilinearRegression.java
+++ src/main/java/app/domain/model/MultilinearRegression.java
@@ -1,32 +1,14 @@
-/*package app.domain.model;
+package app.domain.model;
 
+import Jama.Matrix;
+import Jama.QRDecomposition;
 
-*//**
- *  The {@code MultipleLinearRegression} class performs a multiple linear regression
- *  on an set of <em>N</em> data points using the model
- *  <em>y</em> = &beta;<sub>0</sub> + &beta;<sub>1</sub> <em>x</em><sub>1</sub> + ... +
- &beta;<sub><em>p</em></sub> <em>x<sub>p</sub></em>,
- *  where <em>y</em> is the response (or dependent) variable,
- *  and <em>x</em><sub>1</sub>, <em>x</em><sub>2</sub>, ..., <em>x<sub>p</sub></em>
- *  are the <em>p</em> predictor (or independent) variables.
- *  The parameters &beta;<sub><em>i</em></sub> are chosen to minimize
- *  the sum of squared residuals of the multiple linear regression model.
- *  It also computes the coefficient of determination <em>R</em><sup>2</sup>.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *//*
 public class MultilinearRegression {
     private final Matrix beta;  // regression coefficients
     private double sse;         // sum of squared
     private double sst;         // sum of squared
 
-    *//**
-     * Performs a linear regression on the data points {@code (y[i], x[i][j])}.
-     * @param  x the values of the predictor variables
-     * @param  y the corresponding values of the response variable
-     * @throws IllegalArgumentException if the lengths of the two arrays are not equal
-     *//*
+
     public MultilinearRegression(double[][] x, double[] y) {
         if (x.length != y.length) {
             throw new IllegalArgumentException("matrix dimensions don't agree");
@@ -64,31 +46,17 @@ public class MultilinearRegression {
 
     }
 
-    *//**
-     * Returns the least squares estimate of &beta;<sub><em>j</em></sub>.
-     *
-     * @param  j the index
-     * @return the estimate of &beta;<sub><em>j</em></sub>
-     *//*
+
     public double beta(int j) {
         return beta.get(j, 0);
     }
 
-    *//**
-     * Returns the coefficient of determination <em>R</em><sup>2</sup>.
-     *
-     * @return the coefficient of determination <em>R</em><sup>2</sup>,
-     *         which is a real number between 0 and 1
-     *//*
+
     public double R2() {
         return 1.0 - sse/sst;
     }
 
-    *//**
-     * Unit tests the {@code MultipleLinearRegression} data type.
-     *
-     * @param args the command-line arguments
-     *//*
+
     public static void main(String[] args) {
         double[][] x = { {  1,  10,  20 },
                 {  1,  20,  40 },
@@ -99,7 +67,7 @@ public class MultilinearRegression {
         double[] y = { 243, 483, 508, 1503, 1764, 2129 };
         MultilinearRegression regression = new MultilinearRegression(x, y);
 
-        StdOut.printf("%.2f + %.2f beta1 + %.2f beta2  (R^2 = %.2f)\n",
+        System.out.printf("%.2f + %.2f beta1 + %.2f beta2  (R^2 = %.2f)\n",
                 regression.beta(0), regression.beta(1), regression.beta(2), regression.R2());
     }
-}*/
+}
